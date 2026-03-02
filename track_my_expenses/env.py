ba0b672a4ee357e0204f44da71c6caca6c0cda17
Ammaarah <b.ammaarah101@gmail.com>
2026-03-02 13:02:28 +0000

    Initial commit for Heroku and GitHub

---
+++ track_my_expenses/env.py
@@ -0,0 +1,8 @@
+# env.py
+# Use this file to store environment variables for your Django project.
+# Example usage in settings.py:
+# import env
+# SECRET_KEY = env.SECRET_KEY
+
+SECRET_KEY = 'your-secret-key-here'
+DEBUG = True
