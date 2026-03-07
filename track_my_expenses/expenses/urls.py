fa0211b68e52c2734d062cbb89d3e68e07367680
Ammaarah <b.ammaarah101@gmail.com>
2026-03-07 16:08:43 +0000

    add register button to nav bar

--- track_my_expenses/expenses/urls.py
+++ track_my_expenses/expenses/urls.py
@@ -1,7 +1,9 @@
+from django.views.generic import RedirectView
 
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
@@ -11,4 +13,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html', next_page=None), name='logout'),
+    path('accounts/login/', RedirectView.as_view(url='/login/', permanent=False)),
 ]
